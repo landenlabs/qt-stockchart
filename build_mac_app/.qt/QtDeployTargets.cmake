8f542726f29a21909901e03c744ad61e1f21089d
Dennis Lang <landenlabs2@gmail.com>
2026-03-30 01:07:07 -0400

    clean up

---
+++ build_mac_app/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_StockChart_FILE /Users/dennislang/opt/projects/qt-projects/stocks/build_app/StockChart.app/Contents/MacOS/StockChart)
+set(__QT_DEPLOY_TARGET_StockChart_TYPE EXECUTABLE)
